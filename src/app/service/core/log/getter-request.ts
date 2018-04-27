3483f1e4b2a08bec7c6710e81d7e8adbabaedf28
marcus.chiu <marcuschiu9@gmail.com>
2018-04-27 11:37:22 -0500

    add untracked files

---
+++ src/app/service/core/log/getter-request.ts
@@ -0,0 +1,11 @@
+import {LogType} from './model/extra/log-type';
+import {Pageable} from '../model/pageable';
+
+export class GetterRequest {
+  millisecondThreshold: number;
+  pageable: Pageable;
+  logType: LogType;
+  searchString: string;
+  directoryID: string;
+  tagID: string;
+}
