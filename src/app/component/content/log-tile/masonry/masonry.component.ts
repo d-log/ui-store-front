5e0c304a04ad6213ac2905d4f0edc6ac2f269fe5
marcus.chiu <marcuschiu9@gmail.com>
2018-04-30 00:03:26 -0500

    Masonry - added edge case for manual window resize

--- src/app/component/content/log-tile/masonry/masonry.component.ts
+++ src/app/component/content/log-tile/masonry/masonry.component.ts
@@ -1,4 +1,4 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
+import {Component, EventEmitter, HostListener, Input, OnInit, Output, ViewChild} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {LogModel} from '../../../../service/core/log/model/log-model';
 import {EventBrokerService} from '../../../../service/event-broker-shared-service/event-broker-service';
@@ -29,20 +29,32 @@ export class MasonryComponent implements OnInit {
   constructor(private _eventBroker: EventBrokerService) {
     this._masonry = null;
     this._eventBroker.listen<boolean>(String(BrokerEvent.NAVIGATION_SIDE_LEFT_STATE_CLOSED), (data: boolean) => {
-      this.layout();
-      setTimeout(() => {
-        this.checkForWhiteSpace();
-      }, 500);
+      this.viewportResize();
     });
     this._eventBroker.listen<boolean>(String(BrokerEvent.NAVIGATION_SIDE_LEFT_STATE_OPENED), (data: boolean) => {
-      this.layout();
-      setTimeout(() => {
-        this.checkForWhiteSpace();
-      }, 500);
+      this.viewportResize();
     });
     this._eventBroker.listen(String(BrokerEvent.CONTENT_SCROLLED), (data: boolean) => {
-      this.checkForWhiteSpace();
+      this.loadModelsIfEmptySpace();
     });
+  }
+
+  private viewportResize() {
+    if (this._masonry !== null) {
+      this._masonry.layout();
+
+      setTimeout(() => {
+        this.loadModelsIfEmptySpace();
+      }, 500); // wait for layoutComplete
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // // masonry auto call layout, so no need to call viewportResize
+    setTimeout(() => {
+      this.loadModelsIfEmptySpace();
+    }, 500); // wait for masonry layoutComplete
   }
 
   ngOnInit() {
@@ -57,18 +69,12 @@ export class MasonryComponent implements OnInit {
     this.moreLogsExist = true;
   }
 
-  layout() {
-    if (this._masonry !== null) {
-      this._masonry.layout();
-    }
-  }
-
   /**
    * checks if more logs exist if so check if there is space in viewport
    */
-  checkForWhiteSpace() {
+  loadModelsIfEmptySpace() {
     if (this.moreLogsExist) {
-      if (this.elementInViewport(this.bottom.nativeElement)) {
+      if (this.isElementInViewport(this.bottom.nativeElement)) {
         this.getMoreLogs.emit(true);
       }
     }
@@ -83,16 +89,12 @@ export class MasonryComponent implements OnInit {
         percentPosition: true,
         gutter: 6,
       });
-      this.onNgMasonryInit(masonry);
-      this.checkForWhiteSpace();
+      this._masonry = masonry;
+      this.loadModelsIfEmptySpace();
     }, 5); // delay for dom update
   }
 
-  onNgMasonryInit(masonry: any) {
-    this._masonry = masonry;
-  }
-
-  elementInViewport(el) {
+  isElementInViewport(el) {
     const viewportOffset = el.getBoundingClientRect();
     const top = viewportOffset.top;
     return top <= window.innerHeight;
