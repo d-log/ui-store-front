3db97b8adb379e1774820dd084189027786b3d35
marcus.chiu <marcuschiu9@gmail.com>
2018-05-04 13:03:03 -0500

    PageNotFound - dynamically change font size

--- src/app/component/content/page-not-found/page-not-found.component.ts
+++ src/app/component/content/page-not-found/page-not-found.component.ts
@@ -1,8 +1,25 @@
-import {Component} from '@angular/core';
+import {AfterViewChecked, Component, ViewChild} from '@angular/core';
 
 @Component({
   templateUrl: './page-not-found.component.html',
   styleUrls: ['./page-not-found.component.css']
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements AfterViewChecked {
+
+  @ViewChild('container') containerElement: any;
+  fontSize: number;
+
+  /**
+   * ngAfterViewChecked() will be called every time change detection runs
+   * https://angular.io/guide/lifecycle-hooks
+   */
+  ngAfterViewChecked() {
+    console.log('changing');
+    this.adjustFontSize();
+  }
+
+  adjustFontSize() {
+    const width = this.containerElement.nativeElement.offsetWidth;
+    this.fontSize = width / 6;
+  }
 }
