c2b26a61047c5a63f9bdad2f6220369b5b7e63d2
marcus.chiu <marcuschiu9@gmail.com>
2018-05-13 23:18:02 -0500

    FileCreate - move tileDisplayIndex into logData

--- src/app/component/content/create-file/data/log-data/type/file-create-log-data-container.component.ts
+++ src/app/component/content/create-file/data/log-data/type/file-create-log-data-container.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {LogData} from '../../../../../../service/core/file/model/extra/data/log/extra/log-data/log-data';
+import {LogFileData} from '../../../../../../service/core/file/model/extra/data/log/log-file-data';
 
 @Component({
   selector: 'app-file-create-log-data-container',
@@ -10,24 +11,18 @@ export class FileCreateLogDataContainerComponent {
   @Output() deleteThisLogData = new EventEmitter<number>();
   @Input() index: number;
   @Input() logData: LogData;
+  @Input() data: LogFileData;
 
   collapse: boolean;
   displayNone: boolean;
   timeout: any;
 
-  displayCSS: boolean;
-
   constructor() {
     this.collapse = false;
     this.displayNone = false;
-    this.displayCSS = false;
   }
 
-  toggleDisplayCSS() {
-    this.displayCSS = !this.displayCSS;
-  }
-
-  // animate collapse then set display to none so sortable.js
+  // animate collapse then set css `display: none;` so sortable.js
   // doesn't ghost animate elements below the selected element
   toggle() {
     if (this.collapse) {
@@ -43,6 +38,27 @@ export class FileCreateLogDataContainerComponent {
   }
 
   onDeleteThisLogData() {
+    this.updateLogDataTileDisplayIndex();
     this.deleteThisLogData.emit(this.index);
+  }
+
+  updateLogDataTileDisplayIndex() {
+    const tileDisplayIndex = this.data.logTypeOverride.tile.logDataToDisplayIndex;
+    if (tileDisplayIndex !== undefined) {
+      if (tileDisplayIndex === this.index) {
+        this.data.logTypeOverride.tile.logDataToDisplayIndex = undefined;
+      } else if (tileDisplayIndex > this.index) {
+        this.data.logTypeOverride.tile.logDataToDisplayIndex--;
+      } // else no need to update logDataToDisplayIndex
+    }
+  }
+
+  onChangeThisLogDataAsTileDisplay(event: Event) {
+    const checked = (<HTMLInputElement>event.srcElement).checked;
+    if (checked) {
+      this.data.logTypeOverride.tile.logDataToDisplayIndex = this.index;
+    } else {
+      this.data.logTypeOverride.tile.logDataToDisplayIndex = undefined;
+    }
   }
 }
