58fac2d38ac532b329588ef0fef3f2c78245f34e
Marcus Chiu <marcuschiu9@gmail.com>
2019-04-01 23:57:05 -0500

    added update_production.sh

---
+++ update_production.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+rm -rf dist/
+git push origin master
+ng build --env=prod
+cd dist
+git init
+git add .
+git commit -am "initial commit"
+git remote add origin git@github.com:d-log/ui-store-front-build.git
+git push origin master -f
+
+ssh -i ~/.ssh/aws-marcuschiu.pem ec2-user@www.marcuschiu.com << EOF
+  rm -rf ui-store-front-build/
+  git clone https://github.com/d-log/ui-store-front-build.git
+EOF
