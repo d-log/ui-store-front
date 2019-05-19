72fb0ed6883ee43b7d549785a337e140d28fb7ef
Marcus Chiu <marcuschiu9@gmail.com>
2019-05-19 12:46:42 -0500

    added stuff

--- bin/update_home.sh
+++ bin/update_home.sh
@@ -13,7 +13,8 @@ git commit -am "initial commit"
 git remote add origin git@github.com:d-log/ui-store-front-build.git
 git push origin master -f
 
-ssh root@192.168.86.28 << EOF
-  rm -rf /var/www/ui-store-front-build/
+ssh root@192.168.86.217 << EOF
+  cd /var/www
+  rm -rf ui-store-front-build/
   git clone https://github.com/d-log/ui-store-front-build.git
 EOF
